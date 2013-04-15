ddd027e90db819f5fe2a1f609d6a4d7c25b48620
alkis <alkis.kalogeris@gmail.com>
2013-04-15 12:06:00 +0300

    to singleton
    den to exw testarei. apla to ebala kai eida na mhn exei suntaktika
    sfalmata.

---
+++ src/java/model/ConnectionPool.java
@@ -0,0 +1,41 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+/**
+ *
+ * @author alkis
+ */
+
+//TROPOS KLHSHS ConnectPool.INSTANCE.getConnection();
+public enum ConnectionPool 
+{
+    INSTANCE;
+    private DataSource conPool;
+    
+    private ConnectionPool() 
+    {
+        try 
+        {
+            InitialContext ic = new InitialContext();
+            conPool = ( DataSource ) ic.lookup("jdbc/PointBase");
+        } catch (NamingException ex) {
+            Logger.getLogger(ConnectionPool.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public Connection getConnection() throws SQLException 
+    {
+        return conPool.getConnection();
+    }
+}
